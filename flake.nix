cb47db086d15d0f9bfd1710faa6a40184e688c22
Nithin S Varrier <nithin486@hotmail.com>
2024-05-26 21:54:20 +0530

    add git and other programs

--- flake.nix
+++ flake.nix
@@ -19,8 +19,7 @@ outputs = inputs@{ nixpkgs, home-manager, ... }: {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.nithin = import ./home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass
+            home-manager.backupFileExtension = "bak";	            # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
           }
         ];
--- home.nix
+++ home.nix
@@ -119,6 +119,54 @@ xdg.configFile = {
     #   org.gradle.daemon.idletimeout=3600000
     # '';
   };
+  programs.git = {
+    enable = true;
+    userName = "Nithin S Varrier";
+    userEmail = "nithin486@hotmail.com";
+  };
+
+  # starship - an customizable prompt for any shell
+  programs.starship = {
+    enable = true;
+    # custom settings
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
+  # alacritty - a cross-platform, GPU-accelerated terminal emulator
+  programs.alacritty = {
+    enable = true;
+    # custom settings
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 12;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    # TODO add your custom bashrc here
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
+
+    # set some aliases, feel free to add more or remove some
+    shellAliases = {
+      k = "kubectl";
+      urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
+      urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+    };
+  };
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. If you don't want to manage your shell through Home
