8b9e16ef0100406f4148903ca88a374cd34f60e3
Nithin S Varrier <nithin486@hotmail.com>
2024-06-19 14:37:32 +0530

    fix: wrong fingerprint driver

--- fingerprint.nix
+++ fingerprint.nix
@@ -5,7 +5,7 @@
 }: {
   services.fprintd.enable = true;
   services.fprintd.tod.enable = true;
-  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-elan;
+  services.fprintd.tod.driver = pkgs.libfprint-tod;
 
   environment.systemPackages = with pkgs; [
     fprintd
