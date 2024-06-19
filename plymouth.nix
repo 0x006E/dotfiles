814baf64842731c779d6d76898e648e87525f74b
Nithin S Varrier <nithin486@hotmail.com>
2024-06-19 13:25:01 +0530

    change plymouth theme

--- plymouth.nix
+++ plymouth.nix
@@ -4,16 +4,30 @@
   ...
 }: {
   boot.kernelParams = [
+    # Silences boot messages
     "quiet"
-    "udev.log_level=0"
+    # Show a splash screen
+    "splash"
+
+    "bgrt_disable"
+    # Silences successfull systemd messages from the initrd
+    "rd.systemd.show_status=false"
+    # Silence systemd version number in initrd
+    "rd.udev.log_level=3"
+    # Silence systemd version number
+    "udev.log_priority=3"
+    # If booting fails drop us into a shell where we can investigate
+    "boot.shell_on_fail"
   ];
   boot.initrd.verbose = false;
   boot.consoleLogLevel = 0;
 
-  boot.plymouth = {
+  boot.plymouth = let
+    theme = "lone";
+  in {
     enable = true;
-    theme = "rings";
-    themePackages = [(pkgs.adi1090x-plymouth-themes.override {selected_themes = ["rings"];})];
+    inherit theme;
+    # themePackages = [(pkgs.adi1090x-plymouth-themes.override {selected_themes = [theme];})];
   };
 
   environment.systemPackages = with pkgs; [
