6478159ad3bcbc0bc75bd8e7550e4de558d9a693
Nithin S Varrier <nithin486@hotmail.com>
2024-06-25 15:53:47 +0530

    feat: add docker incus

--- home.nix
+++ home.nix
@@ -117,6 +117,7 @@
     ff2mpv-rust
     firefox
     hoppscotch
+    stirling-pdf
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
--- virtualization.nix
+++ virtualization.nix
@@ -7,6 +7,8 @@
   programs.virt-manager.enable = true;
   services.qemuGuest.enable = true;
   services.spice-webdavd.enable = true;
-
+  virtualisation.incus.enable = true;
+  virtualisation.docker.enable = true;
+  networking.nftables.enable = true;
   users.groups.libvirtd.members = ["nithin"];
 }
