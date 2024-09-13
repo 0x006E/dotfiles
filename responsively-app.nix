979c0aa75b3a2ac0e937c8325e9fff4128ddc792
Restyled.io <commits@restyled.io>
2024-09-13 01:14:59 +0000

    Restyled by nixfmt

--- responsively-app.nix
+++ responsively-app.nix
@@ -1,8 +1,5 @@
-{
-  pkgs,
-  lib,
-  ...
-}: let
+{ pkgs, lib, ... }:
+let
   pname = "responsivelyapp";
   version = "1.12.0";
   name = "${pname}-${version}";
@@ -12,26 +9,26 @@
     sha256 = "sha256-qW6vEOAUZVHdNmn8QWmBGksIjYXez0IGei/AYrxn1VQ=";
   };
 
-  appimageContents = pkgs.appimageTools.extractType2 {inherit name src;};
+  appimageContents = pkgs.appimageTools.extractType2 { inherit name src; };
 in
-  pkgs.appimageTools.wrapType2 rec {
-    inherit name src;
+pkgs.appimageTools.wrapType2 rec {
+  inherit name src;
 
-    extraInstallCommands = ''
-      mv $out/bin/${name} $out/bin/${pname}
-      install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
+  extraInstallCommands = ''
+    mv $out/bin/${name} $out/bin/${pname}
+    install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
 
-      install -m 444 -D ${appimageContents}/${pname}.png $out/share/icons/hicolor/512x512/apps/${pname}.png
+    install -m 444 -D ${appimageContents}/${pname}.png $out/share/icons/hicolor/512x512/apps/${pname}.png
 
-      substituteInPlace $out/share/applications/${pname}.desktop \
-      	--replace 'Exec=AppRun --no-sandbox %U' 'Exec=${pname} %U'
-    '';
+    substituteInPlace $out/share/applications/${pname}.desktop \
+    	--replace 'Exec=AppRun --no-sandbox %U' 'Exec=${pname} %U'
+  '';
 
-    meta = with lib; {
-      description = " A must-have devtool for web developers for quicker responsive web development. 🚀 ";
-      homepage = "https://responsively.app/";
-      license = licenses.gpl3;
-      maintainers = [];
-      platforms = ["x86_64-linux"];
-    };
-  }
+  meta = with lib; {
+    description = " A must-have devtool for web developers for quicker responsive web development. 🚀 ";
+    homepage = "https://responsively.app/";
+    license = licenses.gpl3;
+    maintainers = [ ];
+    platforms = [ "x86_64-linux" ];
+  };
+}
