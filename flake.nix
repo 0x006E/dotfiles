6d883ecf719433ab70b893070d6ac85acb11bad6
Nithin S Varrier <nithin486@hotmail.com>
2024-10-23 10:09:33 +0530

    fix: typo

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "System Configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-kuglimon.url = "github:kuglimon/nixpkgs/vtsls";
