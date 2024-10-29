580f658842df3edc2f6fe8b6fc1011d336e92a57
Nithin S Varrier <nithin486@hotmail.com>
2024-10-29 23:31:12 +0530

    flake: update

--- configuration.nix
+++ configuration.nix
@@ -190,6 +190,7 @@ in
   environment.systemPackages = with pkgs; [
     commit-mono
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    kvmtool
     xwayland
     wget
     libnotify
