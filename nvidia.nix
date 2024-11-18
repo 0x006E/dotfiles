8cda5c74a99c6af3f1f82e7a7e592a167912a98d
Nithin S Varrier <nithin486@hotmail.com>
2024-11-18 14:00:42 +0530

    fix: make nvidia beta for 6_12

--- nvidia.nix
+++ nvidia.nix
@@ -24,7 +24,7 @@
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = true;
 
-    # Use the NVidia open source kernel module (not to be confused with the
+    # Use the NVidia open source nernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
     # Support is limited to the Turing and later architectures. Full list of
     # supported GPUs is at:
@@ -38,7 +38,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
 
     prime = {
       offload.enable = true;
