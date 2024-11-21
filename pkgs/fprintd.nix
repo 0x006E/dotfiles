9a83a11272d0163ed56b5962f16a8deeb0a501dd
Nithin S Varrier <nithin486@hotmail.com>
2024-11-22 00:00:58 +0530

    fprintd: unstable -> 1.94.4
    Diff: https://gitlab.freedesktop.org/libfprint/fprintd/-/compare/refs/tags/vunstable...v1.94.4

--- pkgs/fprintd.nix
+++ pkgs/fprintd.nix
@@ -1,13 +1,22 @@
 {
   pkgs,
   libfprint,
+  fetchFromGitLab,
   ...
 }:
 (pkgs.fprintd.override {
   libfprint = libfprint;
 }).overrideAttrs
-  (oldAttrs: {
+  (oldAttrs: rec {
 
+    src = fetchFromGitLab {
+      domain = "gitlab.freedesktop.org";
+      owner = "libfprint";
+      repo = "fprintd";
+      rev = "refs/tags/v${version}";
+      hash = "sha256-B2g2d29jSER30OUqCkdk3+Hv5T3DA4SUKoyiqHb8FeU=";
+    };
+    version = "1.94.4";
     mesonCheckFlags = oldAttrs.mesonCheckFlags ++ [
       # PAM related checks are timing out
       "--no-suite"
