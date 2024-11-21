ca8cc4141561a22cde75849de95f9197e73b3c7b
Nithin S Varrier <nithin486@hotmail.com>
2024-11-21 22:55:10 +0530

    fix: magazine.nvim

--- pkgs/vimPlugins/magazine.nix
+++ pkgs/vimPlugins/magazine.nix
@@ -1,10 +1,11 @@
 { pkgs, fetchFromGitHub, ... }:
 
-pkgs.vimPlugins.nvim-cmp.overrideAttrs (old: {
+pkgs.vimUtils.buildVimPlugin {
+  name = "magazine.nvim";
   src = fetchFromGitHub {
     owner = "iguanacucumber";
     repo = "magazine.nvim";
     rev = "4aec249cdcef9b269e962bf73ef976181ee7fdd9";
     hash = "sha256-qobf9Oyt9Voa2YUeZT8Db7O8ztbGddQyPh5wIMpK/w8=";
   };
-})
+}
