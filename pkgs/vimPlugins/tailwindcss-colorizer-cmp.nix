8f33a66e638aabdb658c38f4c0f09c84fa3d19f2
Nithin S Varrier <nithin486@hotmail.com>
2024-11-21 23:51:40 +0530

    tailwindcss-colorizer-cmp: unstable -> 0-unstable-2024-03-23
    Diff: https://github.com/roobert/tailwindcss-colorizer-cmp.nvim/compare/3d3cd95e4a4135c250faf83dd5ed61b8e5502b86...3d3cd95e4a4135c250faf83dd5ed61b8e5502b86

--- pkgs/vimPlugins/tailwindcss-colorizer-cmp.nix
+++ pkgs/vimPlugins/tailwindcss-colorizer-cmp.nix
@@ -2,6 +2,7 @@
 
 pkgs.vimUtils.buildVimPlugin {
   name = "tailwindcss-colorizer-cmp";
+  version = "0-unstable-2024-03-23";
   src = fetchFromGitHub {
     owner = "roobert";
     repo = "tailwindcss-colorizer-cmp.nvim";
