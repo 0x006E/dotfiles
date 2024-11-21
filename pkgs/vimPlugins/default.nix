f614a50a464b239c2f4d067b0207b92d0ac9d8d4
Nithin S Varrier <nithin486@hotmail.com>
2024-11-21 23:18:41 +0530

    fix: nvim-cmp is being still used somewhere.

--- pkgs/vimPlugins/default.nix
+++ pkgs/vimPlugins/default.nix
@@ -4,7 +4,7 @@ let
 in
 {
   format-on-save = callPackage ./format-on-save.nix { };
-  magazine = callPackage ./magazine.nix { };
+  nvim-cmp = callPackage ./magazine.nix { };
   remote-nvim = callPackage ./remote-nvim.nix { };
   tailwindcss-colorizer-cmp = callPackage ./tailwindcss-colorizer-cmp.nix { };
   tiny-inline-diagnostic = callPackage ./tiny-inline-diagnostic.nix { };
