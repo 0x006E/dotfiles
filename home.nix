c52eec848fcb38a1980e7f714977b562602434a7
Nithin S Varrier <nithin486@hotmail.com>
2024-12-11 17:21:10 +0530

    fix: qemu typo

--- home.nix
+++ home.nix
@@ -313,8 +313,8 @@ in
 
   dconf.settings = {
     "org/virt-manager/virt-manager/connections" = {
-      autoconnect = [ "qemu=///system" ];
-      uris = [ "qemu=///system" ];
+      autoconnect = [ "qemu:///system" ];
+      uris = [ "qemu:///system" ];
     };
   };
 }
