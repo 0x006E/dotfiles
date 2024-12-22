069eb6af8efda59349057b268c377b33d27edebc
Nithin S Varrier <nithin486@hotmail.com>
2024-12-22 09:02:30 +0530

    fix: set nvidia correct bus id

--- nvidia.nix
+++ nvidia.nix
@@ -45,7 +45,7 @@
       offload.enableOffloadCmd = true;
       # Make sure to use the correct Bus ID values for your system!
       intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
+      nvidiaBusId = "PCI:2:0:0";
     };
   };
   boot.kernelParams = [ "nvidia.NVreg_EnableS0ixPowerManagement=1" ];
