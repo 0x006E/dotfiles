e80b8b0a30e087cbbcda6fa93ac085ded2dc5293
Nithin S Varrier <nithin486@hotmail.com>
2026-07-12 13:41:44 +0530

    feat: ignore router DHCP DNS and globally configure NextDNS DoT and Tailscale MagicDNS

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -14,8 +14,20 @@ delib.module {
       imports = [ inputs.erosanix.nixosModules.protonvpn ];
       networking = {
         hostName = "ntsv";
-        networkmanager.enable = true;
-        nameservers = [ "100.100.100.100" ];
+        networkmanager = {
+          enable = true;
+          connectionConfig = {
+            "ipv4.ignore-auto-dns" = "yes";
+            "ipv6.ignore-auto-dns" = "yes";
+          };
+        };
+        nameservers = [
+          "100.100.100.100"
+          "45.90.28.0#8361b6.dns.nextdns.io"
+          "45.90.30.0#8361b6.dns.nextdns.io"
+          "2a07:a8c0::83:61b6#8361b6.dns.nextdns.io"
+          "2a07:a8c1::83:61b6#8361b6.dns.nextdns.io"
+        ];
         firewall.enable = false;
         firewall.checkReversePath = false;
       };
@@ -26,6 +38,9 @@ delib.module {
       };
 
       services.tailscale.enable = true;
-      services.resolved.enable = true;
+      services.resolved = {
+        enable = true;
+        dnsovertls = "opportunistic";
+      };
     };
 }
